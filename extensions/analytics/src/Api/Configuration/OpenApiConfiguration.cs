430dc520e6f9272d2ada2df0f8bbaa7ea1567032
Samuel Rioja <samuelriotz@hotmail.com>
2025-12-26 20:32:46 -0400

    Remove Scalar package reference and add custom documentation endpoint for Analytics API

--- extensions/analytics/src/Api/Configuration/OpenApiConfiguration.cs
+++ extensions/analytics/src/Api/Configuration/OpenApiConfiguration.cs
@@ -1,5 +1,3 @@
-using Scalar.AspNetCore;
-
 namespace Api.Configuration;
 
 public static class OpenApiConfiguration
@@ -14,7 +12,29 @@ public static class OpenApiConfiguration
     public static WebApplication UseOpenApiConfiguration(this WebApplication app)
     {
         app.MapOpenApi();
-        app.MapScalarApiReference();
+
+        app.MapGet("/docs", () => Results.Content("""
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <title>Analytics API</title>
+                <meta charset="utf-8"/>
+                <script type="module" src="https://unpkg.com/rapidoc/dist/rapidoc-min.js"></script>
+            </head>
+            <body>
+                <rapi-doc
+                    spec-url="/analytics-api/openapi/v1.json"
+                    theme="dark"
+                    bg-color="#1a1a2e"
+                    text-color="#eaeaea"
+                    primary-color="#00d4aa"
+                    render-style="focused"
+                    show-header="false"
+                    allow-try="true"
+                ></rapi-doc>
+            </body>
+            </html>
+            """, "text/html"));
 
         return app;
     }
